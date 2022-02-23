a1ad1d4ebaa97496352c2adf6a3df63b25e3472e
Daniel Brandao <Daniel.26.ramos@gmail.com>
2022-02-23 20:06:17 -0300

    segundo commit

--- src/contas/Conta.java
+++ src/contas/Conta.java
@@ -11,4 +11,22 @@ public class Conta {
 	protected void exibir_saldo() {
 		System.out.println("Saldo: R$ " + saldo);
 	}
+	
+	void sacar (double valor) {
+		saldo -= valor;
+		System.out.println("Débito: R$" + valor);
+		
+	}
+	
+	void deposito (double valor) {
+		saldo += valor;
+		System.out.println("Credito: R$" + valor);
+	}
+	
+	void transferir (Conta destino, double valor) {
+		this.sacar(valor);
+		destino.deposito(valor);
+		System.out.println("Transferência no valor de R$ " + valor + "Realizada com sucessp!");
+		
+	}
 }
--- src/contas/Pessoa_fisica.java
+++ src/contas/Pessoa_fisica.java
@@ -8,6 +8,8 @@ public class Pessoa_fisica {
 		cc1.saldo = 10000;
 		System.out.println("Cliente: " + cc1.cliente);
 		cc1.exibir_saldo();
+		cc1.sacar(1500);
+		cc1.exibir_saldo();
 		System.out.println("---------------------------------------------");
 		
 		Conta cc2 = new Conta();
@@ -15,6 +17,19 @@ public class Pessoa_fisica {
 		cc2.saldo = 2500;
 		System.out.println("Cliente: " + cc2.cliente);
 		cc2.exibir_saldo();
+		cc2.deposito(2500);
+		cc2.exibir_saldo();
+		System.out.println("---------------------------------------------");
+		System.out.println("Tranferencia");
+		System.out.println("Cliente: " + cc1.cliente);
+		System.out.println("Favorecido: " + cc2.cliente);
+		cc1.transferir(cc2, 2000);
+		System.out.println("");
+		System.out.println("Cliente: " + cc1.cliente);
+		cc1.exibir_saldo();
+		System.out.println("Cliente: " + cc2.cliente);
+		cc2.exibir_saldo();
+		
 		
 	}
 
