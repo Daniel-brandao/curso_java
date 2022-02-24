cbfe76e6648eb574e83c0a06b58c080ee0eba962
Daniel Brandao <Daniel.26.ramos@gmail.com>
2022-02-23 21:35:06 -0300

    Calculadora de porcentagem

---
+++ src/exercicios/Excercicio_2_calculador_de_porcentagem.java
@@ -0,0 +1,28 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Excercicio_2_calculador_de_porcentagem {
+
+	public static void main(String[] args) {
+		double vl_inicial, porcent, result;
+		
+		Scanner entra_dados = new Scanner(System.in);
+		
+		System.out.println("Calculador de porcentagem");
+		System.out.print("Digite o valor inicial: ");
+		vl_inicial = entra_dados.nextDouble();
+		System.out.print("Digite a porcentagem que deseja calcular: ");
+		porcent = entra_dados.nextDouble();
+		
+		result = (porcent * vl_inicial )/100;
+		
+		System.out.println(porcent + "% de " + vl_inicial + " é " + result);
+		entra_dados.close();
+		
+		
+		
+
+	}
+
+}
