f8f0eadbb0680d8a604fa06da77e6e62e7179e62
Daniel-brandao <Daniel.26.ramos@gmail.com>
2022-02-28 13:55:02 -0300

    Aprendendo interfaces java

--- src/interface_jframe/Interface_jframe_1.java
+++ src/interface_jframe/Interface_jframe_1.java
@@ -2,13 +2,14 @@ package interface_jframe;
 
 import java.awt.Panel;
 
-import javax.swing.*;
+import javax.swing.JFrame;
 
 public class Interface_jframe_1 {
 
 	public static void main(String[] args) {
 		 JFrame frame = new JFrame("Usuário");
-		 panel painel = new Panel();
+		 Panel panel = new Panel();
+		 frame.add(panel);
 		 frame.setVisible(true);
 		 frame.setSize(400, 400);
 
