cdeab41ec0330c8e5bb8d494c6893289a612074c
Daniel-brandao <Daniel.26.ramos@gmail.com>
2022-02-28 13:51:54 -0300

    Começando interface

---
+++ src/interface_jframe/Interface_jframe_1.java
@@ -0,0 +1,18 @@
+package interface_jframe;
+
+import java.awt.Panel;
+
+import javax.swing.*;
+
+public class Interface_jframe_1 {
+
+	public static void main(String[] args) {
+		 JFrame frame = new JFrame("Usuário");
+		 panel painel = new Panel();
+		 frame.setVisible(true);
+		 frame.setSize(400, 400);
+
+	}
+	
+
+}
